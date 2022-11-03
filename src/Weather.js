148265c74cfb451c82c10e7eca95bd10d2c7eeee
MariaPanchenko <marysa2502@gmail.com>
2022-11-03 22:07:46 +0100

    add link

--- src/Weather.js
+++ src/Weather.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Weather.css';
 // import axios from 'axios';
 import ReactAnimatedWeather from 'react-animated-weather';
+import WeekDay from './WeekDay';
 
 export default function Weather() {
   // function handleSubmit(response) {
@@ -21,108 +22,91 @@ export default function Weather() {
   };
 
   return (
-    <div className="Weather">
-      <form className="mb-3">
-        <div className="row search-form">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Type a city.."
-              className="form-control"
-              autoComplete="off"
-            />
-          </div>
-          <div className=" col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100 submit-search"
-            />
-          </div>
-        </div>
-      </form>
-      <div className="text-center main-content">
-        <div className="overview">
-          <h1>{weatherData.city}</h1>
-          <ul>
-            {/* <li>Last updated: {weatherData.date}</li> */}
-            <li>{weatherData.description}</li>
-          </ul>
-        </div>
-        <div className="row">
-          <div className="col-6">
-            <div className="clearfix weather-temperature">
-              <img
-                src={weatherData.imgUrl}
-                alt={weatherData.description}
-                className="float-left"
+    <div>
+      <div className="Weather">
+        <form className="mb-3">
+          <div className="row search-form">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Type a city.."
+                className="form-control"
+                autoComplete="off"
               />
-              <div className="float-left">
-                <strong className="temp">{weatherData.temperature}</strong>
-                <br />
-                <br />
-                <br />
-                <span className="units">
-                  <a href="/">°C</a> | <a href="/">°F</a>
-                </span>
-              </div>
+            </div>
+            <div className=" col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100 submit-search"
+              />
             </div>
           </div>
-          <div className="col-6">
+        </form>
+        <div className="text-center main-content">
+          <div className="overview">
+            <h1>{weatherData.city}</h1>
             <ul>
-              <li className="weater-status">
-                Humidity: {weatherData.humidity}%
-              </li>
-              <li className="weater-status">Wind: {weatherData.wind} km/h</li>
+              {/* <li>Last updated: {weatherData.date}</li> */}
+              <li>{weatherData.description}</li>
             </ul>
           </div>
+          <div className="row">
+            <div className="col-6">
+              <div className="clearfix weather-temperature">
+                <img
+                  src={weatherData.imgUrl}
+                  alt={weatherData.description}
+                  className="float-left"
+                />
+                <div className="float-left">
+                  <strong className="temp">{weatherData.temperature}</strong>
+                  <br />
+                  <br />
+                  <br />
+                  <span className="units">
+                    <a href="/">°C</a> | <a href="/">°F</a>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <ul>
+                <li className="weater-status">
+                  Humidity: {weatherData.humidity}%
+                </li>
+                <li className="weater-status">Wind: {weatherData.wind} km/h</li>
+              </ul>
+            </div>
+          </div>
+          <hr></hr>
         </div>
-        <hr></hr>
+        <div className="bottom-list">
+          <ul>
+            <li className="bottom-lists">Weather</li>
+            <li className="bottom-lists">News</li>
+            <li className="bottom-lists">Photos</li>
+          </ul>
+        </div>
+        <div className="bottom-content">
+          <ul className="weather-day">
+            <WeekDay label="Monday" temp="+13" iconType="CLEAR_DAY" />
+            <WeekDay label="Tuesday" temp="+16" iconType="WIND" />
+            <WeekDay label="Wednesday" temp="+15" iconType="FOG" />
+          </ul>
+        </div>
       </div>
-      <div className="bottom-list">
-        <ul>
-          <li className="bottom-lists">Weather</li>
-          <li className="bottom-lists">News</li>
-          <li className="bottom-lists">Photos</li>
-        </ul>
-      </div>
-      <div className="bottom-content">
-        <ul className="weather-day">
-          <li>Monday</li>
-          <li>Tuesday</li>
-          <li>Wednesday</li>
-        </ul>
-        <ul className="weather-temp">
-          <li>+21</li>
-          <li>+19</li>
-          <li>+23</li>
-        </ul>
-        <ul className="weather-icons">
-          <li>
-            <ReactAnimatedWeather
-              icon="CLEAR_DAY"
-              color="goldenrod"
-              size={28}
-              animate={true}
-            />
-          </li>
-          <li>
-            <ReactAnimatedWeather
-              icon="WIND"
-              color="goldenrod"
-              size={28}
-              animate={true}
-            />
-          </li>
-          <li>
-            <ReactAnimatedWeather
-              icon="FOG"
-              color="goldenrod"
-              size={28}
-              animate={true}
-            />
-          </li>
-        </ul>
+      <div>
+        <small>
+          <a
+            className="link-git"
+            href="https://github.com/MariaPanchenko/weather-react-app1"
+            target="_blank"
+          >
+            open-source code
+          </a>
+          by Mariia Panchenko
+        </small>
       </div>
     </div>
   );
---
+++ src/WeekDay.js
@@ -0,0 +1,19 @@
+import './WeekDay.css';
+import ReactAnimatedWeather from 'react-animated-weather';
+
+function WeekDay({ label, temp, iconType }) {
+  return (
+    <li className="WeekDay">
+      <span className="week-day-label">{label}</span>
+      <span className="week-day-temp">{temp}</span>
+      <ReactAnimatedWeather
+        icon={iconType}
+        color="goldenrod"
+        size={28}
+        animate={true}
+      />
+    </li>
+  );
+}
+
+export default WeekDay;
